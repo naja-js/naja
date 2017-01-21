ab69c621cdf161005e47fd32c652fc9c2343724f
Jiří Pudil <me@jiripudil.cz>
2017-01-21 16:11:02 +0100

    Naja: implemented makeRequest()

--- src/Naja.js
+++ src/Naja.js
@@ -26,4 +26,35 @@ export default class Naja extends EventTarget {
 		const evt = Object.assign(args, {type, cancelable: true});
 		return this.dispatchEvent(evt);
 	}
+
+
+	makeRequest(method, url, data, options) {
+		const defaultOptions = {
+			dataType: 'post',
+			responseType: 'auto',
+		};
+
+		options = objectAssign({}, defaultOptions, options || {});
+
+		let currentXhr;
+		const beforeCallback = xhr => {
+			currentXhr = xhr;
+			this.fireEvent('before', {xhr, method, url, data, options});
+		};
+
+		const request = qwest.map(method, url, data, options, beforeCallback)
+			.then((xhr, response) => {
+				this.fireEvent('success', {xhr, response});
+				this.fireEvent('complete', {error: null, xhr, response});
+				this.load();
+			})
+			.catch((error, xhr, response) => {
+				this.fireEvent('error', {error, xhr, response});
+				this.fireEvent('complete', {error, xhr, response});
+				this.load();
+			});
+
+		this.fireEvent('start', {request, xhr: currentXhr});
+		return request;
+	}
 }
