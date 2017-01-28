b10e69dbae9032890415b7d11a0b6e330ce0e876
Jiří Pudil <me@jiripudil.cz>
2017-01-28 21:59:04 +0100

    HistoryHandler component

---
+++ src/core/HistoryHandler.js
@@ -0,0 +1,80 @@
+import Component from '../Component';
+
+
+export default class HistoryHandler extends Component {
+	popped = false;
+	href = null;
+	initialState = null;
+
+	constructor(naja) {
+		super(naja);
+		naja.addEventListener('init', this.initialize.bind(this));
+		naja.addEventListener('before', this.saveUrl.bind(this));
+		naja.addEventListener('success', this.pushNewState.bind(this));
+	}
+
+	initialize() {
+		this.popped = !!window.history.state;
+		const initialUrl = window.location.href;
+
+		window.addEventListener('popstate', e => {
+			const state = e.state || this.initialState;
+			const initialPop = !this.popped && initialUrl === state.href;
+			this.popped = true;
+
+			if (initialPop) {
+				return;
+			}
+
+			if (!!state.ui) {
+				this.handleSnippets(state.ui);
+				this.handleTitle(state.title);
+			}
+		});
+
+		window.history.replaceState(this.initialState = {
+			href: window.location.href,
+			title: window.document.title,
+			ui: this.findSnippets(),
+		}, window.document.title, window.location.href);
+	}
+
+	saveUrl({url}) {
+		this.href = url;
+	}
+
+	pushNewState({response}) {
+		if (response.postGet && response.url) {
+			this.href = response.url;
+		}
+
+		history.pushState({
+			href: this.href,
+			title: window.document.title,
+			ui: this.findSnippets(),
+		}, window.document.title, this.href);
+
+		this.href = null;
+		this.popped = true;
+	}
+
+	findSnippets() {
+		const result = {};
+		window.document.querySelectorAll('[id^="snippet-"]').forEach(snippet => {
+			if (!snippet.getAttribute('data-history-nocache')) {
+				result[snippet.id] = snippet.innerHTML;
+			}
+		});
+
+		return result;
+	}
+
+	handleSnippets(snippets) {
+		this.naja.snippetHandler.updateSnippets(snippets);
+		this.naja.load();
+	}
+
+	handleTitle(title) {
+		window.document.title = title;
+	}
+}
---
+++ tests/Naja.HistoryHandler.js
@@ -0,0 +1,137 @@
+import jsdom from './jsdomRegister';
+import {assert} from 'chai';
+import sinon from 'sinon';
+
+
+describe('HistoryHandler', function () {
+	jsdom();
+
+	beforeEach(function (done) {
+		this.Naja = require('../src/Naja').default;
+		this.HistoryHandler = require('../src/core/HistoryHandler').default;
+		done();
+	});
+
+	it('registered in Naja.initialize()', function () {
+		const naja = new this.Naja();
+		naja.initialize();
+		assert.instanceOf(naja.historyHandler, this.HistoryHandler);
+	});
+
+	it('constructor()', function () {
+		const naja = new this.Naja();
+		const mock = sinon.mock(naja);
+
+		mock.expects('addEventListener')
+			.withExactArgs('init', sinon.match.instanceOf(Function))
+			.once();
+
+		mock.expects('addEventListener')
+			.withExactArgs('before', sinon.match.instanceOf(Function))
+			.once();
+
+		mock.expects('addEventListener')
+			.withExactArgs('success', sinon.match.instanceOf(Function))
+			.once();
+
+		new this.HistoryHandler(naja);
+		mock.verify();
+	});
+
+	it('saves initial state', function () {
+		const naja = new this.Naja();
+		const historyHandler = new this.HistoryHandler(naja);
+		historyHandler.initialize();
+
+		assert.deepEqual({
+			href: 'http://example.com/',
+			title: '',
+			ui: {},
+		}, window.history.state);
+	});
+
+	it('pushes new state after successful request', function (done) {
+		const naja = new this.Naja();
+		naja.initialize();
+
+		const el = document.createElement('div');
+		el.id = 'snippet-history-foo';
+		document.body.appendChild(el);
+
+		naja.makeRequest('GET', '/foo').then(() => {
+			assert.equal(2, window.history.length);
+			assert.deepEqual({
+				href: '/foo',
+				title: '',
+				ui: {
+					'snippet-history-foo': 'foo'
+				},
+			}, window.history.state);
+
+			done();
+		});
+
+		this.requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify({snippets: {'snippet-history-foo': 'foo'}}));
+	});
+
+	it('uses the url from payload if postGet is present', function (done) {
+		const naja = new this.Naja();
+		naja.initialize();
+
+		naja.makeRequest('GET', '/foo').then(() => {
+			assert.equal(2, window.history.length);
+			assert.deepEqual({
+				href: '/bar',
+				title: '',
+				ui: {},
+			}, window.history.state);
+
+			done();
+		});
+
+		this.requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify({url: '/bar', postGet: true}));
+	});
+
+	it('redraws snippets on popstate', function () {
+		const naja = new this.Naja();
+		naja.initialize();
+
+		const el = document.createElement('div');
+		el.id = 'snippet-history-foo';
+		document.body.appendChild(el);
+
+		window.dispatchEvent(new PopStateEvent('popstate', {
+			bubbles: false,
+			cancelable: false,
+			state: {
+				href: '/qux',
+				title: 'new title',
+				ui: {
+					'snippet-history-foo': 'foo bar baz',
+				},
+			},
+		}));
+
+		assert.equal(document.title, 'new title');
+		assert.equal(el.innerHTML, 'foo bar baz');
+	});
+
+	it('does not trigger on initial pop', function () {
+		const naja = new this.Naja();
+		naja.initialize();
+
+		const el = document.createElement('div');
+		el.id = 'snippet-history-foo';
+		el.innerHTML = 'foo';
+		document.body.appendChild(el);
+
+		window.dispatchEvent(new PopStateEvent('popstate', {
+			bubbles: false,
+			cancelable: false,
+			state: null,
+		}));
+
+		assert.equal(document.title, '');
+		assert.equal(el.innerHTML, 'foo');
+	});
+});
