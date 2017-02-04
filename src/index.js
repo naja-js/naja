7f0c84b717df81c0984b830698ca3fd13122d3bc
Jiří Pudil <me@jiripudil.cz>
2017-02-04 14:48:41 +0100

    Bundle extensions in default build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Naja from './Naja';
+import AbortExtension from './extensions/AbortExtension';
+import UniqueExtension from './extensions/UniqueExtension';
 
 
 const naja = new Naja();
+naja.registerExtension(AbortExtension);
+naja.registerExtension(UniqueExtension);
+
 document.addEventListener('DOMContentLoaded', naja.initialize.bind(naja));
 
 window.Naja = naja;
