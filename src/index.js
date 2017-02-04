2a5c55e02f233a0656c5ad1984ce08796665e628
Jiří Pudil <me@jiripudil.cz>
2017-02-04 19:45:07 +0100

    Bundle: do not auto-initialize naja

--- src/index.js
+++ src/index.js
@@ -7,7 +7,4 @@ const naja = new Naja();
 naja.registerExtension(AbortExtension);
 naja.registerExtension(UniqueExtension);
 
-document.addEventListener('DOMContentLoaded', naja.initialize.bind(naja));
-
-window.Naja = naja;
 export default naja;
