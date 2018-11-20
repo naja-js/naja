6be3dde6b7c8c543ea3bbd5559b1ab624cc9200d
Jiří Pudil <me@jiripudil.cz>
2018-11-20 22:39:04 +0100

    chore: Migrate build to rollup

---
+++ rollup.config.js
@@ -0,0 +1,69 @@
+const babel = require('rollup-plugin-babel');
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+const {uglify} = require('rollup-plugin-uglify');
+
+const pkg = require('./package.json');
+const output = {
+	banner: `/*\n * Naja.js\n * ${pkg.version}\n *\n * by Jiří Pudil <https://jiripudil.cz>\n */`,
+	sourcemap: true,
+};
+
+const babelPlugin = babel({
+	exclude: 'node_modules/**',
+	runtimeHelpers: true,
+});
+
+export default [
+	{
+		// ESM build for modern tools like webpack
+		input: 'src/index.js',
+		output: {
+			...output,
+			file: pkg.module,
+			format: 'esm',
+		},
+		external: [
+			...Object.keys(pkg.dependencies || {}),
+			...Object.keys(pkg.peerDependencies || {}),
+		],
+		plugins: [
+			resolve(),
+			babelPlugin,
+			commonjs(),
+		],
+	},
+	{
+		// minified
+		input: 'src/index.js',
+		output: {
+			...output,
+			file: pkg.main,
+			format: 'umd',
+			name: 'naja',
+		},
+		external: Object.keys(pkg.peerDependencies || {}),
+		plugins: [
+			resolve(),
+			babelPlugin,
+			commonjs(),
+			uglify(),
+		],
+	},
+	{
+		// non-minified
+		input: 'src/index.js',
+		output: {
+			...output,
+			file: pkg.main.replace('.min', ''),
+			format: 'umd',
+			name: 'naja',
+		},
+		external: Object.keys(pkg.peerDependencies || {}),
+		plugins: [
+			resolve(),
+			babelPlugin,
+			commonjs(),
+		],
+	},
+];
