631386743a85111fc4842a95c6e0bea411561012
Jiří Pudil <me@jiripudil.cz>
2019-11-09 12:50:14 +0000

    Core: do not use named exports so that UMD build works out of the box

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './polyfills';
 
-import {HttpError, Naja} from './Naja';
+import {Naja} from './Naja';
 import {AbortExtension} from './extensions/AbortExtension';
 import {UniqueExtension} from './extensions/UniqueExtension';
 
@@ -9,5 +9,4 @@ const naja = new Naja();
 naja.registerExtension(AbortExtension);
 naja.registerExtension(UniqueExtension);
 
-export {HttpError, Naja};
 export default naja;
