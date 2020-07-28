4c32fc08837fbf340408b213f9002884c6a8d6da
Jiří Pudil <me@jiripudil.cz>
2020-07-28 13:08:00 +0200

    chore: simpler Babel setup

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,9 @@ module.exports = (api) => {
 
 	return {
 		presets: [
-			['@babel/preset-env', {
-				useBuiltIns: 'usage',
-				corejs: '3',
-			}],
+			'@babel/preset-env',
 		],
 		plugins: [
-			['@babel/plugin-transform-runtime', {
-				'useESModules': true,
-			}],
 			'@babel/plugin-proposal-class-properties',
 			'@babel/plugin-proposal-nullish-coalescing-operator',
 			'@babel/plugin-proposal-optional-chaining',
