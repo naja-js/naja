12413a3c4b52cb939f6d97c88b0ffb5dd90f3d44
Jiří Pudil <me@jiripudil.cz>
2020-08-31 14:12:50 +0200

    types: export Options and Payload types for better extensibility

--- src/index.esm.d.ts
+++ src/index.esm.d.ts
@@ -2,5 +2,5 @@ import {Naja} from './Naja';
 
 declare const naja: Naja;
 
-export {Naja, Extension, HttpError} from './Naja';
+export {Naja, Options, Payload, Extension, HttpError} from './Naja';
 export default naja;
