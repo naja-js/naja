6e5587ceee435043493dc8f5418f4a695f1b4267
Jiří Pudil <me@jiripudil.cz>
2020-10-29 10:04:20 +0100

    chore: fix coverage reporting

--- babel.config.js
+++ babel.config.js
@@ -6,22 +6,7 @@ module.exports = (api) => {
 			'@babel/preset-env',
 		],
 		plugins: [
-			'@babel/plugin-proposal-class-properties',
-			'@babel/plugin-proposal-nullish-coalescing-operator',
-			'@babel/plugin-proposal-optional-chaining',
 			'@babel/plugin-transform-runtime',
 		],
-		env: {
-			development: {
-				plugins: [
-					['babel-plugin-istanbul', {
-						exclude: [
-							"node_modules/**",
-							"tests/**/*.js"
-						],
-					}],
-				],
-			},
-		},
 	};
 };
