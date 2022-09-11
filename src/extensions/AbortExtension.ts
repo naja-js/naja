a57dbd34b6cecc29b295631e925b68412f905afc
Jiří Pudil <me@jiripudil.cz>
2022-09-11 17:22:36 +0200

    AbortExtension: refactor to support multiple concurrent requests

--- src/extensions/AbortExtension.ts
+++ src/extensions/AbortExtension.ts
@@ -1,4 +1,4 @@
-import {BeforeEvent, Extension, Naja, StartEvent} from '../Naja';
+import {CompleteEvent, Extension, Naja, StartEvent} from '../Naja';
 import {InteractionEvent} from '../core/UIHandler';
 
 declare module '../Naja' {
@@ -11,44 +11,44 @@ export class AbortExtension implements Extension {
 	public initialize(naja: Naja): void {
 		naja.uiHandler.addEventListener('interaction', this.checkAbortable.bind(this));
 		naja.addEventListener('init', this.onInitialize.bind(this));
-		naja.addEventListener('before', this.checkAbortable.bind(this));
 		naja.addEventListener('start', this.saveAbortController.bind(this));
-		naja.addEventListener('complete', this.clearAbortController.bind(this));
+		naja.addEventListener('complete', this.removeAbortController.bind(this));
 	}
 
 
-	private abortable: boolean = true;
-	private abortController: AbortController | null = null;
+	private readonly abortControllers: Set<AbortController> = new Set();
+
 	private onInitialize(): void {
 		document.addEventListener('keydown', (event) => {
-			if (this.abortController !== null
-				&& event.key === 'Escape'
-				&& !(event.ctrlKey || event.shiftKey || event.altKey || event.metaKey)
-				&& this.abortable
-			) {
-				this.abortController.abort();
-				this.abortController = null;
+			if (event.key === 'Escape' && !(event.ctrlKey || event.shiftKey || event.altKey || event.metaKey)) {
+				for (const controller of this.abortControllers) {
+					controller.abort();
+				}
+
+				this.abortControllers.clear();
 			}
 		});
 	}
 
-	private checkAbortable(event: InteractionEvent | BeforeEvent): void {
-		const {options} = event.detail;
-		this.abortable = 'element' in event.detail
-			? (event.detail.element.getAttribute('data-naja-abort') ?? (event.detail.element as HTMLInputElement).form?.getAttribute('data-naja-abort')) !== 'off' // eslint-disable-line no-extra-parens
-			: options.abort !== false;
-
-		// propagate to options if called in interaction event
-		options.abort = this.abortable;
+	private checkAbortable(event: InteractionEvent): void {
+		const {element, options} = event.detail;
+		if (element.hasAttribute('data-naja-abort') || (element as HTMLInputElement).form?.hasAttribute('data-naja-abort')) {
+			options.abort = (element.getAttribute('data-naja-abort') ?? (element as HTMLInputElement).form?.getAttribute('data-naja-abort')) !== 'off';
+		}
 	}
 
 	private saveAbortController(event: StartEvent): void {
-		const {abortController} = event.detail;
-		this.abortController = abortController;
+		const {abortController, options} = event.detail;
+		if (options.abort !== false) {
+			this.abortControllers.add(abortController);
+			options.clearAbortExtension = () => this.abortControllers.delete(abortController);
+		}
 	}
 
-	private clearAbortController(): void {
-		this.abortController = null;
-		this.abortable = true;
+	private removeAbortController(event: CompleteEvent): void {
+		const {options} = event.detail;
+		if (options.abort !== false && !!options.clearAbortExtension) {
+			options.clearAbortExtension();
+		}
 	}
 }
