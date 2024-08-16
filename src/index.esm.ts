00c3bbc1a28b16cf300061dedffca346bca6a690
Jiří Pudil <me@jiripudil.cz>
2024-08-16 10:17:43 +0200

    TS: export events and other auxiliary types (closes #408)

--- src/index.esm.ts
+++ src/index.esm.ts
@@ -7,5 +7,15 @@ const naja = new Naja();
 naja.registerExtension(new AbortExtension());
 naja.registerExtension(new UniqueExtension());
 
+export type {InitEvent, BeforeEvent, StartEvent, AbortEvent, PayloadEvent, SuccessEvent, ErrorEvent, CompleteEvent} from './Naja';
+export type {BuildStateEvent, RestoreStateEvent} from './core/HistoryHandler';
+export type {RedirectEvent} from './core/RedirectHandler';
+export type {StoreEvent, FetchEvent, RestoreEvent} from './core/SnippetCache';
+export type {BeforeUpdateEvent, PendingUpdateEvent, AfterUpdateEvent} from './core/SnippetHandler';
+export type {InteractionEvent} from './core/UIHandler';
+
+export type {HistoryState} from './core/HistoryHandler';
+export type {Extension, Options, Payload} from './Naja';
+
 export {Naja, HttpError} from './Naja';
 export default naja;
